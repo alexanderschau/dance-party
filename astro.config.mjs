231565df207985688387c7c9030c4d5e32facf8d
Alexander Schau <alexander@schau.email>
2022-04-30 20:51:31 +0000

    add floating gallery

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
@@ -6,5 +6,5 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), sitemap()]
+  integrations: [svelte(), tailwind(), sitemap()],
 });
---
+++ src/components/RowAnimation.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  export let imageUrls: string[];
+</script>
+
+<div class="my-8 w-max">
+  <div class="bg inline-block h-1/2">
+    {#each imageUrls as image}
+      <img
+        alt="a nice party gif :("
+        src={"https://media.giphy.com/media/" + image + "/giphy.gif"}
+        loading="lazy"
+        class="mx-4 inline-block h-full"
+      />
+    {/each}
+  </div>
+  <div class="bg inline-block h-1/2">
+    {#each imageUrls as image}
+      <img
+        alt="a nice party gif :("
+        src={"https://media.giphy.com/media/" + image + "/giphy.gif"}
+        loading="lazy"
+        class="mx-4 inline-block h-full"
+      />
+    {/each}
+  </div>
+</div>
+
+<style>
+  .bg {
+    transform: translateY(0);
+    animation: background-shift 30s linear infinite forwards;
+  }
+  @keyframes background-shift {
+    to {
+      transform: translateX(-100%);
+    }
+  }
+</style>
